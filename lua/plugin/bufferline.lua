15883631e9a76e0eb6cae57de76da8d53616d906
Caden Shmookler <cshmookler@gmail.com>
2024-05-11 14:53:51 -0600

    Removed tab indicators

--- lua/plugin/bufferline.lua
+++ lua/plugin/bufferline.lua
@@ -30,7 +30,7 @@ return function()
             show_buffer_icons = true,             -- disable filetype icons for buffers
             show_buffer_close_icons = false,
             show_close_icon = false,
-            show_tab_indicators = true,
+            show_tab_indicators = false,
             show_duplicate_prefix = true,    -- whether to show duplicate buffer prefix
             duplicates_across_groups = true, -- whether to consider duplicate paths in different groups as duplicates
             persist_buffer_sort = true,      -- whether or not custom sorted buffers should persist
