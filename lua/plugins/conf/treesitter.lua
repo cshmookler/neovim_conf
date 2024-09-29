84557f31a54c36cc5bbb5683dcb6a1a03d53fa12
Caden Shmookler <cshmookler@gmail.com>
2024-09-28 23:49:00 -0600

    Install all treesitter parsers on startup.

--- lua/plugins/conf/treesitter.lua
+++ lua/plugins/conf/treesitter.lua
@@ -2,6 +2,7 @@ return function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
         auto_install = true,
+        ensure_installed = "maintained",
         highlight = {
             enable = true,
             additional_vim_regex_highlighting = false,
