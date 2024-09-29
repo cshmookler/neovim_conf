665e3747a99b66283d0481c11fa6a141f1d10e8b
Caden Shmookler <cshmookler@gmail.com>
2024-09-29 00:00:12 -0600

    FIX: removed the 'ensure_installed' field from the treesitter config because 'maintained' is not a valid value.

--- lua/plugins/conf/treesitter.lua
+++ lua/plugins/conf/treesitter.lua
@@ -2,7 +2,6 @@ return function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
         auto_install = true,
-        ensure_installed = "maintained",
         highlight = {
             enable = true,
             additional_vim_regex_highlighting = false,
